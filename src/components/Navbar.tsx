eb8683e1228d9a00cc2b0679bc2f5ab575bef90c
Karl George <karlgdev@gmail.com>
2023-08-08 16:33:10 +0200

    Working on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+import { Icons } from './Icons';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='fixed inset-x-0 top-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
+      <div className='container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl'>
+        <Link href='/' className='flex items-center gap-2'>
+          <Icons.logo className='w-8 h-8 sm:h-6 sm:w-6' />
+          <p className='hidden text-sm font-medium text-zinc-700 md:block'>
+            Readit
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
