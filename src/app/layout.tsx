b267e397202cadbf2f51b7ec9b7d37a8755a7515
Karl George <karlgdev@gmail.com>
2023-08-08 16:22:16 +0200

    Adding font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import '@/styles/globals.css';
+import { cn } from '@/lib/utils';
+import { Inter } from 'next/font/google';
 
 export const metadata = {
   title: 'Readit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 };
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -11,8 +15,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang='en'
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+    >
+      <body className='min-h-screen pt-12 antialiased bg-slate-50'>
+        <Navbar />
+        <div className='container h-full pt-12 mx-auto max-w-7xl'>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
