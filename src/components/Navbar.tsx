d4afd24c4cba477c0e42687085051496b87b7a84
Karl George <karlgdev@gmail.com>
2023-08-16 12:06:28 +0200

    Working on Signin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link';
 import { Icons } from './Icons';
+import { buttonVariants } from './ui/Button';
 
 const Navbar = () => {
   return (
     <div className='fixed inset-x-0 top-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl'>
+        {/* Logo */}
         <Link href='/' className='flex items-center gap-2'>
           <Icons.logo className='w-8 h-8 sm:h-6 sm:w-6' />
           <p className='hidden text-sm font-medium text-zinc-700 md:block'>
             Readit
           </p>
+        </Link>
+
+        {/* Searchbar */}
+
+        {/* Auth */}
+        <Link href='/sign-in' className={buttonVariants()}>
+          Sign In
         </Link>
       </div>
     </div>
