fe6a496ec3b7138cdcf939767af1334fb5762bab
Karl George <karlgdev@gmail.com>
2023-08-17 14:53:35 +0200

    adding dropdown menu items

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@radix-ui/react-dropdown-menu';
+} from '@/components/ui/DropdownMenu';
 import { User } from 'next-auth';
 import UserAvatar from './UserAvatar';
+import Link from 'next/link';
+import { signOut } from 'next-auth/react';
 
 interface UserAccountNavProps {
   user: Pick<User, 'name' | 'image' | 'email'>;
@@ -18,6 +25,41 @@ function UserAccountNav({ user }: UserAccountNavProps) {
           user={{ name: user.name || null, image: user.image || null }}
         />
       </DropdownMenuTrigger>
+      <DropdownMenuContent className='bg-white' align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-1 leading-none'>
+            {user.name && <p className='font-medium'>{user.name}</p>}
+            {user.email && (
+              <p className='w-[200px] truncate text-sm text-zinc-700'>
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href='/'>Feed</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href='/r/create'>Create Community</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href='/settings'>Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem
+          onSelect={(event) => {
+            event.preventDefault();
+            signOut({ callbackUrl: `${window.location.origin}/sign-in` });
+          }}
+          className='cursor-pointer'
+        >
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,5 +1,5 @@
 import { User } from 'next-auth';
-import { Avatar, AvatarFallback } from './ui/avatar';
+import { Avatar, AvatarFallback } from './ui/Avatar';
 import Image from 'next/image';
 import { Icons } from './Icons';
 import { AvatarProps } from '@radix-ui/react-avatar';
